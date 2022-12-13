889da79fd3cfd63ad32b294f4f6b519d0cba4334
Richard <kilted2000@hotmail.com>
2022-12-12 23:04:53 -0600

    add profile pic

--- app.js
+++ app.js
@@ -13,31 +13,22 @@ app.use(express.static("public"));
 app.get("/", (req,res)=>{
   res.render("index");
 })
-app.get("/", (req,res)=>{
+app.get("/contact", (req,res)=>{
     res.render("contact");
   })
 
-  app.get("/", (req,res)=>{
+  app.get("/projects", (req,res)=>{
     res.render("projects");
   })
 
-  app.get("/", (req,res)=>{
+  app.get("/success", (req,res)=>{
     res.render("success");
   })
 
-  app.get("/", (req,res)=>{
+  app.get("/about", (req,res)=>{
     res.render("about");
   })
 
-  app.get("/posts/:postName", (req,res)=>{
-    let requestedPage = _.lowerCase(req.params.postName);
-   posts.forEach((post)=>{
-     let storedTitle = _.lowerCase(post.title);
-    if( requestedPage === storedTitle)
-       res.render("post", {title: post.title, content: post.content});
-   }) 
-    }
-   );
 
 app.listen(3000, ()=>{
     console.log("Server running on Port 3000.");
