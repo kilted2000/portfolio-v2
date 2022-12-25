2d3cc20570e6622a7f9d51a14615d0deb0d35da0
Richard <kilted2000@hotmail.com>
2022-12-25 14:12:07 -0800

    chnage date

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const ejs = require("ejs");
 const _ = require("lodash");
 const bodyParser = require("body-parser");
+const favicon = require('serve-favicon');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -10,25 +12,32 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
 app.get("/", (req,res)=>{
   res.render("index");
 })
 app.get("/contact", (req,res)=>{
-    res.render("contact");
+  const day = date.getDate();
+    res.render("contact", {latha: day});
   })
 
   app.get("/projects", (req,res)=>{
-    res.render("projects");
+    const day = date.getDate();
+    res.render("projects", {latha: day});
   })
 
   app.get("/success", (req,res)=>{
-    res.render("success");
+    const day = date.getDate();
+    res.render("success", {latha: day});
   })
 
   app.get("/about", (req,res)=>{
-    res.render("about");
+    const day = date.getDate();
+    res.render("about", {latha: day});
   })
-
+//  app.get("/site.webmanifest)",(req,res)=>{
+// res.render("site.webmanifest");
+//  })
 
 app.listen(3000, ()=>{
     console.log("Server running on Port 3000.");
